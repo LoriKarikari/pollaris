167d00e2753333a7e66663dfc717d5275b6dd451
Lori Karikari <lori.karikari@gmail.com>
2023-04-27 09:08:39 +0200

    add basic Twitch login

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	semi: false,
+	singleQuote: true,
+	printWidth: 100,
 };
 
 module.exports = config;
